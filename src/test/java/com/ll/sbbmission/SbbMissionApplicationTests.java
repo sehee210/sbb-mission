8222df4a6d98e4c45a6cb1020195b092438c25a7
unknown <berryggomo>
2023-11-23 22:23:50 +0900

    2-05

--- src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/ll/sbbmission/SbbMissionApplicationTests.java
@@ -1,13 +1,34 @@
 package com.ll.sbbmission;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
-class SbbMissionApplicationTests {
+class SbbApplicationTests {
+    @Autowired
+    private QuestionRepository questionRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @Transactional
+    @Test
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
 
+        List<Answer> answerList = q.getAnswersList();
+
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+    }
 }
