36158c00b8b46b806159a8b4a08a05bfe54d077c
sehee210 <sehee0210@gmail.com>
2023-11-28 13:38:42 +0900

    3-10

--- src/main/java/com/ll/sbbmission/answer/AnswerService.java
+++ src/main/java/com/ll/sbbmission/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.ll.sbbmission.answer;
 
+import com.ll.sbbmission.DataNotFoundException;
 import com.ll.sbbmission.question.Question;
 import com.ll.sbbmission.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -19,5 +21,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if(answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
