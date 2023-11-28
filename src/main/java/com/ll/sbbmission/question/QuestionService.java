c58170b7905bf8eb306b380933d80686505d9f2c
sehee210 <sehee0210@gmail.com>
2023-11-28 10:31:17 +0900

    3-08

--- src/main/java/com/ll/sbbmission/question/QuestionService.java
+++ src/main/java/com/ll/sbbmission/question/QuestionService.java
@@ -1,6 +1,7 @@
 package com.ll.sbbmission.question;
 
 import com.ll.sbbmission.DataNotFoundException;
+import com.ll.sbbmission.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -31,11 +32,12 @@ public class QuestionService {
         }
     }
 
-    public void create(String subject, String content) {
+    public void create(String subject, String content, SiteUser user) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
+        q.setAuthor(user);
         this.questionRepository.save(q);
     }
 
